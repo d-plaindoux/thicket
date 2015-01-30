da68e70de76bf0cd742acff447726d648508c698
Didier Plaindoux <d.plaindoux@free.fr>
2015-01-30 19:20:26 +0100

    Brute force compiler in progress / POC only

--- test/Movico/compiler_test.js
+++ test/Movico/compiler_test.js
@@ -1,9 +1,7 @@
 'use strict';
 
-/*
 var ast = require('../../lib' + (process.env.MOVICO_COV || '') + '/Movico/ast.js').ast,
     compiler = require('../../lib' + (process.env.MOVICO_COV || '') + '/Movico/compiler.js').compiler;
-*/
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -33,8 +31,44 @@ exports['compile'] = {
   'Simple model': function (test) {
       test.expect(1);
       
-      // compiler.model(ast.model("A",[],[]));
-      test.ok(true);
+      test.equal(compiler.model(ast.model("A",[],[])).success(),
+                 "var mvc$A = {$ : 'A'};");
+      test.done();
+  },
+
+  'Model with one attribute': function (test) {
+      test.expect(1);
+      
+      test.equal(compiler.model(ast.model("A",[],[ast.param("a",ast.type.native("a"))])).success(),
+                 "var mvc$A = function (mvc$a) { return {$ : 'A', 'a': mvc$a}};");
+      test.done();
+  },
+
+  'Model with two attributes': function (test) {
+      test.expect(1);
+      
+      test.equal(compiler.model(ast.model("A",[],[ast.param("a1",ast.type.native("a")),
+                                                  ast.param("a2",ast.type.native("b"))])).success(), 
+                 "var mvc$A = function (mvc$a1) { return function (mvc$a2) { return {$ : 'A', 'a1': mvc$a1, 'a2': mvc$a2}}};");
+      test.done();
+  },
+    
+  'Simple controller': function (test) {
+      test.expect(1);
+      
+      test.equal(compiler.controller([], ast.controller("A",[],ast.param("this",ast.type.native("a")),[],[])).success(),
+                 "var mvc$A = function (mvc$this) { return {$: 'A'}; };");
+      test.done();
+  },
+    
+  'Controller with unbox': function (test) {
+      test.expect(1);
+      
+      test.equal(compiler.controller([], ast.controller("A",[],
+                                                        ast.param("this",ast.type.native("a")),
+                                                        [],
+                                                        [ast.method("unbox", ast.expr.ident("this"))])).success(),
+                 "var mvc$A = function (mvc$this) { return {$: 'A', 'unbox': mvc$this}; };");
       test.done();
   },
 };
