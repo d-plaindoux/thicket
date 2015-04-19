a61f5548106bfa6c8ba3258d076305e8110736e1
Didier Plaindoux <d.plaindoux@free.fr>
2015-04-19 23:08:27 +0200

    Dependencies are now also generated for a better compilation support

--- test/Runtime/repl_test.js
+++ test/Runtime/repl_test.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var movicoc = require('../../lib' + (process.env.MOVICO_COV || '') + '/Movico/movicoc.js'),
+    codegen = require('../../lib' + (process.env.MOVICO_COV || '') + '/Movico/generator/code.js'),    
     native = require('../../lib' + (process.env.MOVICO_COV || '') + '/Runtime/native.js'),
     M = native(require('../../lib' + (process.env.MOVICO_COV || '') + '/Runtime/runtime.js'));
 
@@ -34,54 +35,60 @@ exports['repl'] = {
   'Number': function(test) {
     test.expect(1);
     // tests here  
-    var expression = movicoc.sentence([], "123");
+    var expression = movicoc.sentence([], "123"),
+        code = codegen.sentence([], expression.success().expr).success();
         
-    test.deepEqual(M.$$(eval(expression.success().code)), M.$$(M.number(123)));
+    test.deepEqual(M.$$(eval(code)), M.$$(M.number(123)));
     test.done();
   },
     
   'String': function(test) {
     test.expect(1);
     // tests here  
-    var expression = movicoc.sentence([], "'123'");
+    var expression = movicoc.sentence([], "'123'"),
+        code = codegen.sentence([], expression.success().expr).success();
         
-    test.deepEqual(M.$$(eval(expression.success().code)), M.$$(M.string("123")));
+    test.deepEqual(M.$$(eval(code)), M.$$(M.string("123")));
     test.done();
   },
     
   'Unit': function(test) {
     test.expect(1);
     // tests here  
-    var expression = movicoc.sentence([], "()");
+    var expression = movicoc.sentence([], "()"),
+        code = codegen.sentence([], expression.success().expr).success();
         
-    test.deepEqual(M.$$(eval(expression.success().code)), M.$$(M.unit));
+    test.deepEqual(M.$$(eval(code)), M.$$(M.unit));
     test.done();
   },
     
   'Identity function': function(test) {
     test.expect(1);
     // tests here  
-    var expression = movicoc.sentence([], "(fun f -> f) 1");
+    var expression = movicoc.sentence([], "(fun f -> f) 1"),
+        code = codegen.sentence([], expression.success().expr).success();
         
-    test.deepEqual(M.$$(eval(expression.success().code)), M.$$(M.number(1)));
+    test.deepEqual(M.$$(eval(code)), M.$$(M.number(1)));
     test.done();
   },
     
   'Left projection function': function(test) {
     test.expect(1);
     // tests here  
-    var expression = movicoc.sentence([], "(fun x y -> x) 1 2");
+    var expression = movicoc.sentence([], "(fun x y -> x) 1 2"),
+        code = codegen.sentence([], expression.success().expr).success();
         
-    test.deepEqual(M.$$(eval(expression.success().code)), M.$$(M.number(1)));
+    test.deepEqual(M.$$(eval(code)), M.$$(M.number(1)));
     test.done();
   },
     
   'Right projection function': function(test) {
     test.expect(1);
     // tests here  
-    var expression = movicoc.sentence([], "(fun x y -> y) 1 2");
+    var expression = movicoc.sentence([], "(fun x y -> y) 1 2"),
+        code = codegen.sentence([], expression.success().expr).success();
         
-    test.deepEqual(M.$$(eval(expression.success().code)), M.$$(M.number(2)));
+    test.deepEqual(M.$$(eval(code)), M.$$(M.number(2)));
     test.done();
   },    
 /*   
