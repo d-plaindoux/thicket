52266285360e25bf1404880b2b97dce51ae467b8
Didier Plaindoux <d.plaindoux@free.fr>
2015-05-28 09:16:09 +0200

    Provide native function tests

--- test/Runtime/native_test.js
+++ test/Runtime/native_test.js
@@ -1,9 +1,7 @@
 'use strict';
 
-/*
 var native = require('../../lib' + (process.env.THICKET_COV || '') + '/Runtime/native.js'),
-    M = native(require('../../lib' + (process.env.THICKET_COV || '') + '/Runtime/runtime.js'));
-*/
+    runtime = require('../../lib' + (process.env.THICKET_COV || '') + '/Runtime/runtime.js').extendWith(native);
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -29,210 +27,349 @@ exports['native'] = {
   setUp: function(done) {
     done();
   },
-/*    
+   
   'numbers true comparison': function(test) {
     test.expect(1);
     // tests here  
-    test.ok(M.delta["generic.=="](M.number(1))(M.number(1))(true)(false));
+    var code = runtime.delta["generic.=="].concat([
+        {CONST:1},{APPLY:1},
+        {CONST:1},{APPLY:1},
+        {CONST:true},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:true});
     test.done();
   },
-
+    
   'numbers false comparison': function(test) {
     test.expect(1);
     // tests here  
-    test.ok(M.delta["generic.=="](M.number(1))(M.number(2))(false)(true));
+    var code = runtime.delta["generic.=="].concat([
+        {CONST:1},{APPLY:1},
+        {CONST:2},{APPLY:1},
+        {CONST:true},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:false});
     test.done();
   },
 
   'strings true comparison': function(test) {
     test.expect(1);
     // tests here  
-    test.ok(M.delta["generic.=="](M.string("1"))(M.string("1"))(true)(false));
-    test.done();
-  },
-
-  'strings false comparison': function(test) {
-    test.expect(1);
-    // tests here  
-    test.ok(M.delta["generic.=="](M.string("1"))(M.string("2"))(false)(true));
+    var code = runtime.delta["generic.=="].concat([
+        {CONST:"1"},{APPLY:1},
+        {CONST:"1"},{APPLY:1},
+        {CONST:true},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:true});
     test.done();
   },
     
+  'strings false comparison': function(test) {
+    test.expect(1);
+    // tests here  
+    var code = runtime.delta["generic.=="].concat([
+        {CONST:"1"},{APPLY:1},
+        {CONST:"2"},{APPLY:1},
+        {CONST:true},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:false});
+    test.done();
+  },
+   
   'numbers true <<': function(test) {
     test.expect(1);
     // tests here  
-    test.ok(M.delta["generic.<<"](M.number(1))(M.number(2))(true)(false));
-    test.done();
-  },
+    var code = runtime.delta["generic.<<"].concat([
+        {CONST:1},{APPLY:1},
+        {CONST:2},{APPLY:1},
+        {CONST:true},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:true});
+    test.done();  },
 
   'numbers false <<': function(test) {
-    test.expect(1);
+      test.expect(1);
     // tests here  
-    test.ok(M.delta["generic.<<"](M.number(1))(M.number(1))(false)(true));
-    test.done();
-  },
-
+    var code = runtime.delta["generic.<<"].concat([
+        {CONST:2},{APPLY:1},
+        {CONST:1},{APPLY:1},
+        {CONST:true},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:false});
+    test.done();  },
+    
   'strings true <<': function(test) {
     test.expect(1);
     // tests here  
-    test.ok(M.delta["generic.<<"](M.string("1"))(M.string("2"))(true)(false));
-    test.done();
+    var code = runtime.delta["generic.<<"].concat([
+        {CONST:"1"},{APPLY:1},
+        {CONST:"2"},{APPLY:1},
+        {CONST:true},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:true});
+    test.done();  
   },
 
   'strings false <<': function(test) {
     test.expect(1);
     // tests here  
-    test.ok(M.delta["generic.<<"](M.string("1"))(M.string("1"))(false)(true));
-    test.done();
+    var code = runtime.delta["generic.<<"].concat([
+        {CONST:"2"},{APPLY:1},
+        {CONST:"1"},{APPLY:1},
+        {CONST:true},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:false});
+    test.done();  
   },
-    
+   
   'strings catenation': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["string.+"](M.string("1"))(M.string("1")), 
-                   M.$$(M.string("11")));
-    test.done();
+    var code = runtime.delta["string.+"].concat([
+        {CONST:"4"},{APPLY:1},
+        {CONST:"2"},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:"42"});
+    test.done();  
   },
     
   'string toNumber': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["string.toNumber"](M.string("1"))(function(n) { return n;})(M.number(-1)),
-                   M.$$(M.number(1)));
-    test.done();
+    var code = runtime.delta["string.toNumber"].concat([
+        {CONST:"4"},{APPLY:1},
+        {CLOSURE:[{ACCESS:1},{RETURN:1}]},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:4});
+    test.done();  
   },
         
   'string wrong toNumber': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["string.toNumber"](M.string("a1"))(function(n) { return n;})(M.string("Fail")),
-                   M.$$(M.string("Fail")));
+    var code = runtime.delta["string.toNumber"].concat([
+        {CONST:"m4"},{APPLY:1},
+        {CLOSURE:[{ACCESS:1},{RETURN:1}]},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:false});
     test.done();
   },
         
   'strings hash': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["string.hash"](M.string("1")),
-                   M.$$(M.number(49)));
+    var code = runtime.delta["string.hash"].concat([
+        {CONST:"4"},{APPLY:1},
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:52});
     test.done();
   },
-   
+ 
   'numbers +': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["number.+"](M.number(6))(M.number(2)),
-                   M.$$(M.number(8)));
+    var code = runtime.delta["number.+"].concat([
+        {CONST:6},{APPLY:1},
+        {CONST:2},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:8});
     test.done();
   },
    
   'numbers -': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["number.-"](M.number(6))(M.number(2)),
-                   M.$$(M.number(4)));
+    var code = runtime.delta["number.-"].concat([
+        {CONST:6},{APPLY:1},
+        {CONST:2},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:4});
     test.done();
   },
    
   'numbers *': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["number.*"](M.number(6))(M.number(2)),
-                   M.$$(M.number(12)));
+    var code = runtime.delta["number.*"].concat([
+        {CONST:6},{APPLY:1},
+        {CONST:2},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:12});
     test.done();
   },
    
   'numbers /': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["number./"](M.number(6))(M.number(2)),
-                   M.$$(M.number(3)));
+    var code = runtime.delta["number./"].concat([
+        {CONST:6},{APPLY:1},
+        {CONST:2},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:3});
     test.done();
   },
    
+  'numbers / by zero': function(test) {
+    test.expect(1);
+    // tests here  
+    var code = runtime.delta["number./"].concat([
+        {CONST:6},{APPLY:1},
+        {CONST:0},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:Infinity});
+    test.done();
+  },
+
   'number toString': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["number.toString"](M.number(6)),
-                   M.$$(M.string("6")));
+    var code = runtime.delta["number.toString"].concat([
+        {CONST:4},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:"4"});
     test.done();
   },
-     
+
   'array new': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["array.new"](M.number(1)),
-                   M.$$(new Array(1)));
+    var code = runtime.delta["array.new"].concat([
+        {CONST:1},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:new Array(1)});
     test.done();
   },
-     
+
   'array set': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["array.set"](M.number(new Array(2)))(M.number(1))(M.string("1")),
-                   M.$$([, M.string("1")]));
+    var code = runtime.delta["array.set"].concat([
+        {CONST:new Array(1)},{APPLY:1},
+        {CONST:0},{APPLY:1},
+        {CONST:1},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:[ {CONST:1} ]});
     test.done();
   },
 
   'array set out of bound': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["array.set"](M.number(new Array(1)))(M.number(1))(M.string("1")),
-                   M.$$(new Array(1)));
+    var code = runtime.delta["array.set"].concat([
+        {CONST:new Array(1)},{APPLY:1},
+        {CONST:1},{APPLY:1},
+        {CONST:1},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:new Array(1)});
     test.done();
   },
-
+    
   'array reset': function(test) {
     test.expect(1);
     // tests here  
-    var array = [ M.number(1) ];
-    test.deepEqual(M.delta["array.reset"](M.number(array))(M.number(0)),
-                   M.$$(new Array(1)));
+    var code = runtime.delta["array.reset"].concat([
+        {CONST:[ {CONST:1} ]},{APPLY:1},
+        {CONST:0},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:new Array(1)});    
     test.done();
   },
 
   'array reset out of bound': function(test) {
     test.expect(1);
     // tests here  
-    var array = [ M.number(1) ];
-    test.deepEqual(M.delta["array.reset"](M.number(array))(M.number(1)),
-                   M.$$([ M.number(1) ]));
+    var code = runtime.delta["array.reset"].concat([
+        {CONST:[ {CONST:1} ]},{APPLY:1},
+        {CONST:1},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:[ {CONST:1} ]});    
     test.done();
   },
 
   'array get': function(test) {
     test.expect(1);
     // tests here  
-    var array = [ M.number(1) ];
-    test.deepEqual(M.delta["array.get"](M.number(array))(M.number(0))(function (a) { return a; })(M.string("Fail")),
-                   M.$$(M.number(1)));
+    var code = runtime.delta["array.get"].concat([
+        {CONST:[ {CONST:1} ]},{APPLY:1},
+        {CONST:0},{APPLY:1},
+        {CLOSURE:[{ACCESS:1},{RETURN:1}]},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:1});    
     test.done();
   },
 
   'array get undefined': function(test) {
     test.expect(1);
     // tests here  
-    var array = new Array(1);
-    test.deepEqual(M.delta["array.get"](M.number(array))(M.number(0))(function (a) { return a; })(M.string("Fail")),
-                   M.$$(M.string("Fail")));
+    var code = runtime.delta["array.get"].concat([
+        {CONST:new Array(1)},{APPLY:1},
+        {CONST:0},{APPLY:1},
+        {CLOSURE:[{ACCESS:1},{RETURN:1}]},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:false});    
     test.done();
   },
 
   'array get out of bound': function(test) {
     test.expect(1);
     // tests here  
-    var array = [ M.number(1) ];
-    test.deepEqual(M.delta["array.get"](M.number(array))(M.number(1))(function (a) { return a; })(M.string("Fail")),
-                   M.$$(M.string("Fail")));
+    var code = runtime.delta["array.get"].concat([
+        {CONST:[ {CONST:1} ]},{APPLY:1},
+        {CONST:1},{APPLY:1},
+        {CLOSURE:[{ACCESS:1},{RETURN:1}]},{APPLY:1},
+        {CONST:false},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:false});    
     test.done();
   },
 
   'array size': function(test) {
     test.expect(1);
     // tests here  
-    test.deepEqual(M.delta["array.size"](M.number(new Array(10))),
-                   M.$$(M.number(10)));
+    var code = runtime.delta["array.size"].concat([
+        {CONST:new Array(1)},{APPLY:1}
+    ]);      
+      
+    test.deepEqual(runtime.execute(code), {CONST:1});    
     test.done();
   },
-*/  
+ 
 };
